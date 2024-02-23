1ed97765c4b6959c82d7c19c2a0e380f5e84e3fd
muselesscreator <bwarzesk@gmail.com>
2024-02-23 13:58:07 -0500

    feat: initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from '@muselesscreator/react-shallow-snapshot';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(shallow(<App />).snapshot).toMatchSnapshot();
 });
