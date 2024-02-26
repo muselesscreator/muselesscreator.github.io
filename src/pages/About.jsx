b57ae0a8b15cf29308befdf130b40822310043c3
muselesscreator <bwarzesk@gmail.com>
2024-02-26 11:45:11 -0500

    fix: typo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <h2 className="h2 article-title">About me</h2>
       </header>
       <div className="about-text">
-        <p>With over a decade of software engineering, I specialize in helping teams produce solid, testable frontend-code and maintainable agile frontend development processes, as well as experience in building and managing communities devoted to frontend communication, coordination, and training.</p>
+        <p>With over a decade of software engineering, I specialize in helping teams produce solid, testable frontend-code and maintainable agile frontend development processes, as well as building and managing communities devoted to frontend communication, coordination, and training.</p>
         <p>I have experience guiding large projects from conception, through ideation and refinement, implementation, validation, release, and maintenance, as well as coming into existing projects and codebases and updating them to a consistent level of quality.</p>
         <p>My background in SQA and the odd variety of engineering tech my career has exposed me to give me a mindset and focus on maintainability of code and practices above all else.  I like to spend a bit of thoughtful effort now to make less work for everyone later.</p>
         <p>My core goal in any code-base is not only to achieve excellent code, but specifically to make achieving excellence as easy as possible.  I have high standards for my code and the projects I own, but I also endeavor to provide tools, workflows, and utilities to allow developers to quickly build well-designed, easily-maintained, and highly/deeply tested code.</p>
